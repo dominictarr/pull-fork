32acfec39326bb0fd6731a5bcd5596b8d29cec77
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-03 02:36:50 +0000

    tests for abort paths

--- test/abort2.js
+++ test/abort2.js
@@ -13,20 +13,20 @@ function error (err) {
 }
 
 ;interleave.test
-(function (async) {
+(function h(async) {
 
   var n = 2
   var err = new Error('closes both streams')
 
-  var ary1, aborted2, ended
+  var aborted1, aborted2, ended, seen = []
   pull(
-    //after the second stream aborts,
-    //the rest of the even numbers should be dropped on the floor.
-    pull.values([1,2,3,4,5,6,7,8]),
+    pull.values([1,2,3,5,7]),
     async.through(),
     function (read) {
       return function (abort, cb) {
         read(abort, function (end, data) {
+          if(data) seen.push(data)
+          console.log('DATA', data)
           if(end) ended = end
           cb(end, data)
         })
@@ -35,14 +35,22 @@ function error (err) {
     fork([
       pull(
         async.through(),
-        pull.collect(function (err, ary) {
-          ary1 = ary
-          done()
-        })),
+        function (read) {
+          read(null, function (err, data) {
+            console.log('abort1')
+            read(true, function () {
+              //abort should wait until all the streams have ended
+              //before calling back.
+              aborted1 = ended
+              done()
+            })
+          })
+        }),
       pull(
         async.through(),
         function (read) {
           read(null, function (err, data) {
+            console.log('abort2')
             read(true, function () {
               //abort should wait until all the streams have ended
               //before calling back.
@@ -50,17 +58,22 @@ function error (err) {
               done()
             })
           })
-        }
-      )],
+        })
+      ],
       function (e) { return (e+1)%2 }
     )
   )
 
   function done(err, ary) {
     if(--n) return
-    console.log(aborted2, ary1)
-    assert.deepEqual(ary1, [1, 3, 5, 7])
-    if(!aborted2 || !ary1)
+    console.log('***********************88')
+    console.log(aborted2, aborted1)
+    //since each sink aborts after 1
+    //item, we should have only read the first two items
+    console.log(seen)
+    assert.deepEqual(seen, [1, 2])
+    assert.ok(ended)
+    if(!aborted1 || !aborted2)
       throw new Error('test failed')
 
     async.done()
