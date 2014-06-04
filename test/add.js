d6fe17b3715b430ddb88a94c28b85a850f67f73b
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-04 00:53:05 +0000

    tests for adding a stream on the fly

---
+++ test/add.js
@@ -0,0 +1,37 @@
+//var tape = require('tape')
+var pull = require('pull-stream')
+var fork = require('../')
+var assert = require('assert')
+var interleave = require('interleavings')
+
+interleave.test(function (async) {
+  var n = 2, results = []
+
+  var f = fork(
+    function (data, sinks) {
+      return data % 2
+    },
+    function (i, sinks) {
+      return pull(
+              async.through(),
+              pull.collect(function (err, value) {
+                if(err) throw err
+                results[i] = value
+                done()
+              }))
+    })
+
+  pull(pull.values([1,2,3,4,5,6,7,8]), async.through(), f)
+
+  var values
+
+  function done () {
+    if(--n) return
+    assert.deepEqual(results, [
+      [2, 4, 6, 8],
+      [1, 3, 5, 7]
+    ])
+    async.done()
+  }
+
+})
